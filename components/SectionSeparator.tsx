17ee3ea35455279003646c647ea67c11ace080d7
davisrukitis <davis@rukitis.com>
2025-03-13 12:31:46 +0200

    Delete components directory

--- components/SectionSeparator.tsx
+++
@@ -1,6 +0,0 @@
-export default function SectionSeparator() {
-  return (
-    <div className="h-32 -mt-16 -mb-16 w-full bg-gradient-to-b from-transparent via-[#efefed]/50 to-transparent"></div>
-  )
-}
-
